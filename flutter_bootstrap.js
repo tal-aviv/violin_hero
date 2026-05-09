1001ce5b39b129a87c9d8cf90e88212bc6e8e0b3
tal arane <talarane@meta.com>
2026-05-09 22:15:03 +0300

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "856686340" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3800724306" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
